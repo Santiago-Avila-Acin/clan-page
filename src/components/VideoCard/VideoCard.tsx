e4057ebe476db32861d4ced9636b346023bea20a
Santiago Avila Acin <Santiavilaacin@hotmail.com>
2023-06-15 22:24:31 -0300

    desktop support V1.0.1

--- src/components/VideoCard/VideoCard.tsx
+++ src/components/VideoCard/VideoCard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import ReactPlayer from "react-player";
 import "./VideoCard.css";
 
@@ -7,10 +8,31 @@ interface VideoCardProps {
 
 export const VideoCard = ({ url }: VideoCardProps) => {
   const isYouTubeUrl = url.includes("youtube.com") || url.includes("youtu.be");
+  const [playerSize, setPlayerSize] = useState({ width: "100%", height: "auto" });
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      if (screenWidth < 458) {
+        setPlayerSize({ width: "80%", height: "16rem" });
+      } else if (screenWidth < 800){
+        setPlayerSize({ width: "80%", height: "30rem" });
+      } else if (screenWidth < 1000){
+        setPlayerSize({ width: "80%", height: "40rem" });
+      }else{
+        setPlayerSize({ width: "70%", height: "56rem" });
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className="video_card__container">
-      {isYouTubeUrl && <ReactPlayer url={url} width="92%" height="20rem" className="single_video" controls />}
+      {isYouTubeUrl && <ReactPlayer url={url} {...playerSize} className="single_video" controls />}
     </div>
   );
 };
